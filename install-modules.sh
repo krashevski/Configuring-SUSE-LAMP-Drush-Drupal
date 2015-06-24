78a3c8159b98bbb6cb025fe8944f7e7a2565a96b
krashevski <v.krashevski@gmail.com>
2015-06-24 19:38:31 +0600

    Update install-modules.sh

--- install-modules.sh
+++ install-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to remove instal Drupal modules
+# Script to remove instal Drupal popular SEO modules
 #
 # Script by Vladislav Krashevskij (v.krashevski#gmail.com)
 # 24.06.2015
@@ -15,31 +15,15 @@ if [[ $_replydir =~ ^(yes|y) ]]; then
     _group='www'
 #
 # Install popular Drupal 7 modules
-    read -p "Do you want to install popular Drupal modules? (y/n): " replymod
+    read -p "Do you want to install popular Drupal SEO modules? (y/n): " replymod
     _replymod=${replymod,,} # # to lower case
     if [[ $_replymod =~ ^(yes|y) ]]; then
-        printf "%s\n" "" "Process of installing popular Drupal modules..." ""
-        drush dl admin_menu, ctools, pathauto, globalredirect, page_title, image_resize_filter, colorbox, jquery_update, xmlsitemap, entity, file_entity, search404
+        printf "%s\n" "" "Process of installing popular Drupal SEO modules..." ""
+        drush dl pathauto, globalredirect, page_title, xmlsitemap, search404
         chown -Rf ${_user}:${_group} sites/all/modules
 # Enabling popular Drupal modules
-        drush en -y admin_menu_toolbar, ctools, pathauto, globalredirect, page_title, image_resize_filter, colorbox, jquery_update, xmlsitemap, file_entity, search404
-        printf "%s\n" "" "Popular Drupal modules are installed." ""
-# Additional libraries
-        printf "%s\n" "" "Process of installing additional libraries Drupal..." ""
-        mkdir -p sites/all/libraries
-#
-# To include a code library external to the Drupal project http://drupal.org/packaging-whitelist
-# Install Colobox librarie
-        cd sites/all/libraries
-        drush colorbox-plugin
-#
-# Install Colobox librarie if not work drush command
-#   wget --no-check-certificate https://github.com/jackmoore/colorbox/tarball/master
-#   unzip master -d /home/${_user}/public_html/${_sitepatch}/${_sitepatch}.lh/sites/all/libraries
-#   cd /home/${_user}/public_html/${_sitepatch}/${_sitepatch}.lh/sites/all/libraries
-#   rename colorbox* colorbox colorbox*
-#
-        printf "%s\n" "" "Additional libraries are installed." ""
+        drush en -y pathauto, globalredirect, page_title, xmlsitemap, search404
+        printf "%s\n" "" "Popular Drupal SEO modules are installed." ""
 #
 # Disconnecting module Drupal shorcut
 #   drush dis toolbar shorcut -y
