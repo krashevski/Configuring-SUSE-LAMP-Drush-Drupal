c78b660c73f0cce50234450fd8241d39a32d7039
krashevski <v.krashevski@gmail.com>
2015-06-26 12:32:04 +0600

    Temporary directory and memcache
    Set or remove strings to config site temporary directory and memcache

--- after-install.sh
+++ after-install.sh
@@ -44,6 +44,23 @@ if [[ $_replydir =~ ^(yes|y) ]]; then
         sed --in-place '/file_temporary_path/d' sites/default/settings.php
     fi
 #
+# Activate Drupal Memcache module https://www.drupal.org/node/1131458
+    read -p "Do you want to activate Drupal Memcache module? (y/n): " replydmem
+    _replydmem=${replydmem,,} # # to lower case
+    if [[ $_replydmem =~ ^(yes|y) ]]; then
+        if ! grep -q 'cache_backends' sites/default/settings.php ; then
+            echo -n $'$conf[\'cache_backends\'][] = \'sites/all/modules/memcache/memcache.inc\';' >> sites/default/settings.php
+            echo -n $'$conf[\'cache_default_class\'] = \'MemCacheDrupal\';' >> sites/default/settings.php
+            echo -n "Enter unique memcache key prefix for this site: "
+            read replyunmem
+            echo -n $'$conf[\'memcache_key_prefix\'] = \'$replyunmem\';' >> sites/default/settings.php
+        fi
+    else
+        sed --in-place '/cache_backends/d' sites/default/settings.php
+        sed --in-place '/cache_default_class/d' sites/default/settings.php
+        sed --in-place '/memcache_key_prefix/d' sites/default/settings.php
+    fi
+#
 # Security settings Drupal site
     chmod 0440 sites/default/settings.php
     chmod 0755 sites/default
